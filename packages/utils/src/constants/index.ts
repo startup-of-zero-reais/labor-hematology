0f8a26388c1b1889725b48c24028a7c367c69c0b
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-04-28 23:30:51 -0300

    feat: added some constants based on domains

--- packages/utils/src/constants/index.ts
+++ packages/utils/src/constants/index.ts
@@ -1,6 +1,14 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Labor Hematology'
-export const SHORT_DOMAIN = process.env.NEXT_PUBLIC_SHORT_DOMAIN || 'lhm.co'
+export const SHORT_DOMAIN =
+	process.env.NEXT_PUBLIC_SHORT_DOMAIN || 'laborator.io'
 export const HOME_DOMAIN = `https://${process.env.NEXT_PUBLIC_APP_DOMAIN}`
+
+export const APP_HOSTNAMES = new Set([
+	`${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+	`preview.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+	`localhost:3000`,
+	`localhost`,
+])
 
 export const APP_DOMAIN =
 	process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
@@ -9,16 +17,33 @@ export const APP_DOMAIN =
 			? `https://preview.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
 			: `http://localhost:3000`
 
-export const APP_HOSTNAMES = new Set([
-	`app.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
-	`preview.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
-	`localhost:3000`,
-	`localhost`,
-])
-
 export const ADMIN_HOSTNAMES = new Set([
 	`admin.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
 	`admin.localhost:3000`,
+])
+
+export const DASHBOARD_HOSTNAMES = new Set([
 	`dashboard.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
 	`dashboard.localhost:3000`,
 ])
+
+export const DASHBOARD_DOMAIN =
+	process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+		? `https://dashboard.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
+		: process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+			? `https://preview-dashboard.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
+			: `http://dashboard.localhost:3000`
+
+export const API_HOSTNAMES = new Set([
+	`api.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+	`api-staging.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+	`api.${SHORT_DOMAIN}`,
+	`api.localhost:3000`,
+])
+
+export const API_DOMAIN =
+	process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+		? `https://api.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
+		: process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+			? `https://api-staging.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
+			: `http://api.localhost:3000`
