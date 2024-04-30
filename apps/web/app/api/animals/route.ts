b1dcf747b9c9233ce7a7259c30bf75722471cba4
Vicente Nascimento <vicente19981@live.com>
2024-04-30 14:40:49 -0300

    feat: adding animal schema on route post

--- apps/web/app/api/animals/route.ts
+++ apps/web/app/api/animals/route.ts
@@ -1,33 +1,13 @@
 import prisma from '@/lib/prisma'
-import { z } from 'zod'
 import { handleAndReturnErrorResponse } from '@/lib/api/errors'
-import { getPagination, handlePaginate } from '@labor/utils'
+import { getPagination, getSearchParams, handlePaginate } from '@labor/utils'
+import { animalSchema } from './schema'
 
 export async function POST(request: Request) {
 	try {
 		const body = await request.json()
 
-		const animal = z
-			.object({
-				name: z.string({ required_error: 'name is required' }),
-				trackingMark: z.string().optional(),
-				markType: z
-					.enum(['MICROCHIP', 'WASHER', 'UNMARKED'], {
-						message:
-							'mark type should be one of MICROCHP, WASHER or UNMARKED',
-					})
-					.optional(),
-				age: z
-					.number({ required_error: 'age should be a number' })
-					.positive({
-						message: 'age should be a positive integer',
-					}),
-				gender: z.enum(['MALE', 'FEMALE', 'UNDEFINED'], {
-					required_error:
-						'gender should be one of MALE, FEMALE or UNDEFINED',
-				}),
-			})
-			.parse(body)
+		const animal = animalSchema.parse(body)
 
 		const result = await prisma.animal.create({
 			data: animal,
@@ -35,7 +15,7 @@ export async function POST(request: Request) {
 				id: true,
 			},
 		})
-		return Response.json(result)
+		return Response.json(result, { status: 201 })
 	} catch (error) {
 		return handleAndReturnErrorResponse(error)
 	}
@@ -44,11 +24,20 @@ export async function POST(request: Request) {
 export async function GET(request: Request) {
 	try {
 		const pagination = getPagination(request.url)
+		const params = getSearchParams(request.url)
 
 		const [animals, count] = await prisma.$transaction([
 			prisma.animal.findMany({
 				take: pagination.limit,
 				skip: pagination.offset,
+				where: {
+					name: {
+						contains: params.name,
+					},
+					trackingMark: {
+						contains: params.trackingMark,
+					},
+				},
 				orderBy: {
 					updatedAt: 'desc',
 				},
